3425c83ff373d1d61b9e7400fe7480e6b1429df2
Ian <219239532+markd3ng@users.noreply.github.com>
2026-06-23 09:20:54 +0800

    feat: add analytics service PUBLIC_* type declarations

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,4 +5,15 @@ interface ImportMetaEnv {
   readonly PUBLIC_BING_SITE_VERIFICATION?: string;
   readonly PUBLIC_YANDEX_SITE_VERIFICATION?: string;
   readonly PUBLIC_BAIDU_SITE_VERIFICATION?: string;
+  readonly PUBLIC_CLARITY_ID?: string;
+  readonly PUBLIC_GA_ID?: string;
+  readonly PUBLIC_ADSENSE_PUBLISHER_ID?: string;
+  readonly PUBLIC_PLAUSIBLE_DOMAIN?: string;
+  readonly PUBLIC_PLAUSIBLE_SRC?: string;
+  readonly PUBLIC_FATHOM_SITE_ID?: string;
+  readonly PUBLIC_FATHOM_SRC?: string;
+  readonly PUBLIC_UMAMI_WEBSITE_ID?: string;
+  readonly PUBLIC_UMAMI_SRC?: string;
+  readonly PUBLIC_MATOMO_SITE_ID?: string;
+  readonly PUBLIC_MATOMO_URL?: string;
 }
